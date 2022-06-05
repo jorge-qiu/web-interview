5f70a3142ee7fc9362250dfecc8656acaca82cb4
qiu <qiou3732580@126.com>
2022-06-05 12:33:28 +0800

    css

---
+++ 1.js/18.es6新特性用法/迭代器.js
@@ -0,0 +1,30 @@
+//定义生成器函数，入参是任意集合
+// es5逻辑
+function iteratorGenerator(list) {
+  //idx记录当前访问的索引
+  var idx = 0;
+  //len记录传入集合的长度
+  var len = list.length;
+  return {
+    //自定义next方法
+    next: function () {
+      //如果索引还没有超出集合长度，done为false
+      var done = idx >= len;
+      //如果done为false,则可以继续取值
+      var value = !done ? list[idx++] : undefined;
+
+      //将当前值与遍历是否完毕（done）返回
+      return {
+        done: done,
+        value: value,
+      };
+    },
+  };
+}
+
+var iterator = iteratorGenerator([1,2,3])
+
+iterator.next()
+iterator.next()
+iterator.next()
+
